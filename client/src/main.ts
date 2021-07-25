540bf24c899dc64e4030c292dc8168863031e230
KG <acrkdduf@naver.com>
2021-07-25 20:58:50 +0900

    sass 및 모듈 import/export 테스트를 위한 파일 생성

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,14 @@
+import './index.scss';
 import { test } from './module';
+import { logo } from './assets';
 
 const $app = document.getElementById('app');
 $app!.innerHTML = 'hello world';
+
+const $back = document.createElement('div');
+$back.classList.add('back');
+$back.style.backgroundImage = `url(${logo})`;
+$app?.append($back);
 
 const arr = [1, 2, 3];
 arr.includes(3);
