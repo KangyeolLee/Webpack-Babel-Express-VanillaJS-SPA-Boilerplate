b3cca0de0eefc688050f554a6a2feec325c431e6
KG <acrkdduf@naver.com>
2021-07-26 19:44:31 +0900

    추가된 테스트 이미지 파일 내보내기 설정

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,2 +1,5 @@
 export { default as addMint } from './add-mint.svg';
 export { default as logo } from './Logoo.png';
+export { default as heartfill } from './heart-fill.svg';
+export { default as tea } from './tea.svg';
+export { default as myom } from './myom_logo7.png';
