634c8430dbff505c56db52f3dededdaec57a7313
KG <acrkdduf@naver.com>
2021-07-27 10:53:58 +0900

    헬퍼 함수 유틸 함수로 작성

--- client/src/utils/helper.ts
+++ client/src/utils/helper.ts
@@ -1,2 +1,16 @@
+import { ClassElement } from 'typescript';
+
 export const html = (str: TemplateStringsArray, ...args: unknown[]) =>
   str.map((s, i) => `${s}${args[i] || ''}`).join('');
+
+export const customEventEmitter = (eventType: string, detail?: object) => {
+  document.dispatchEvent(
+    new CustomEvent(eventType, {
+      detail,
+    })
+  );
+};
+
+export function isClass(value: ClassElement) {
+  return Boolean(value && value.toString().startsWith('class '));
+}
