1706e7718c7cbfdfc713b870d01feac19f9104df
KG <acrkdduf@naver.com>
2021-08-08 23:31:39 +0900

    express loader 파일 작성

---
+++ server/loader/express.loader.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+export default ({ app }: { app: express.Application }) => {
+  app.set('port', config.port || 3000);
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const err = new Error('Not Found') as any;
+    err.status = 404;
+    next(err);
+  });
+
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.log(err);
+    res.status(err.status || 500).json({
+      error: err.message,
+    });
+  });
+};
